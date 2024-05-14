0088c2c1c00911b242e8debe2edaf9e67544392f
Moein Tavakoli <mtawakoli2283@gmail.com>
2024-05-14 12:45:00 +0330

    [ Feature ] Add main - Add flag to pass arg as dir

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import yaml
+import sys
+from argparse import ArgumentParser
 
-# Directory to find *.rules 
-# Be sure that all ruels ends with .rules
-rules_directory = './rules'
-
+# Function to extract teams and severities from a YAML file
 def extract_teams_and_severities(file_path):
     with open(file_path) as file:
         data = yaml.safe_load(file)
@@ -23,20 +22,33 @@ def extract_teams_and_severities(file_path):
 
     return teams_and_severities
 
-yaml_files = [file for file in os.listdir(rules_directory) if file.endswith('.rules')]
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Process alert rules.')
+    parser.add_argument('-d', '--directory', dest='rules_directory', default='./rules',
+                        help='directory containing alert rules')
+    args = parser.parse_args()
 
-# List to store teams and severities from all files
-all_teams_and_severities = []
+    rules_directory = args.rules_directory
 
-# Iterate over each YAML file and extract teams and severities
-for yaml_file in yaml_files:
-    file_path = os.path.join(rules_directory, yaml_file)
-    teams_and_severities = extract_teams_and_severities(file_path)
-    all_teams_and_severities.extend(teams_and_severities)
+    if os.path.exists(rules_directory):
+        print(f'rules_directory = {rules_directory}')
+        # List all YAML files in the directory
+        yaml_files = [file for file in os.listdir(rules_directory) if file.endswith('.rules')]
+        
+        # List to store teams and severities from all files
+        all_teams_and_severities = []
 
-# Remove duplicates
-all_teams_and_severities = [dict(t) for t in {tuple(d.items()) for d in all_teams_and_severities}]
+        # Iterate over each YAML file and extract teams and severities
+        for yaml_file in yaml_files:
+            file_path = os.path.join(rules_directory, yaml_file)
+            teams_and_severities = extract_teams_and_severities(file_path)
+            all_teams_and_severities.extend(teams_and_severities)
 
-print("List of teams and severities from all rules:")
-for item in all_teams_and_severities:
-    print(f"Team: {item['team']}, Severity: {item['severity']}")
+        # Remove duplicates
+        all_teams_and_severities = [dict(t) for t in {tuple(d.items()) for d in all_teams_and_severities}]
+
+        print("List of teams and severities from all rules:")
+        for item in all_teams_and_severities:
+            print(f"Team: {item['team']}, Severity: {item['severity']}")
+    else:
+        raise Exception("No such file or directory!")
